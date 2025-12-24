0b460fd68e7749a413442ebe6c29e29879816608
seonghoon jeong <jeongseonghoon4537@gmail.com>
2025-12-24 14:25:44 +0900

    임의 키 경로도 홈으로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ExamPage from './pages/ExamPage';
-import SolutionPage from './pages/SolutionPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ExamPage from "./pages/ExamPage";
+import SolutionPage from "./pages/SolutionPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 임의 키 경로도 홈으로 */}
+        <Route path="/:key" element={<HomePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/exam/:examId" element={<ExamPage />} />
         <Route path="/solution/:examId" element={<SolutionPage />} />
